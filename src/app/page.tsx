675dd55fdca0ced12f1d620f2a5a6c6a5f59148f
NIYIGENA Patrick <pattywrld2003@gmail.com>
2026-04-10 12:28:44 +0200

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
       <h1 className="text-4xl font-bold mb-4">Umurava AI</h1>
-      <p className="text-xl text-gray-600">Building the future of recruitment with AI.</p>
+      <p className="text-xl text-gray-600">Building the future of recruitment with Artificail Intergence.</p>
     </main>
 
   );
